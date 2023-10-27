6d4eb782a75bdc3a2670e006d7c0ef51742dc138
crisrodriguezgar <rodriguezgar.cris@gmail.com>
2023-10-27 14:10:12 +0200

    added styles landing

---
+++ src/components/Card.jsx
@@ -0,0 +1,55 @@
+import defaultAvatar from '../assets/avatar.png';
+
+const Card = ({data, autorLanding}) => {
+    const avatarAutor = data.image === '' ? defaultAvatar : data.image;
+  return (
+    <section className={`preview_autor ${autorLanding}`}>
+        <section className="infoProject">
+          <div className="infoProject_container">
+            <hr className="infoProject_container-line1" />
+            <p className="infoProject_container-subtitle">
+              Personal Project Card
+            </p>
+            <hr className="infoProject_container-line2" />
+          </div>
+
+          <h2 className="infoProject_title">
+            {data.name || 'Elegant Workspace'}
+          </h2>
+          <p className="infoProject_slogan">
+            {data.slogan || 'Diseños Exclusivos'}
+          </p>
+          <p className="infoProject_desc">
+            {data.desc ||
+              `Gestión de proyectos en línea, diseñado para simplificar y agilizar tu trabajo como programadora. Con solo crear una tarjeta para cada proyecto, podrás organizarlos de manera eficiente. ¡Optimiza tu productividad y alcanza tus metas de manera efectiva con nosotras!`}
+          </p>
+          <section className="technologies">
+            <p className="technologies_text">
+              {data.technologies || 'React JS - HTML - CSS'}
+            </p>
+            <a href={data.demo} target="_blank" rel="noreferrer">
+              <button className="technologies_icon">
+                <i className="fa-solid fa-globe technologies_icon-img"></i>
+              </button>
+            </a>
+            <a href={data.repo} target="_blank" rel="noreferrer">
+              <button className="technologies_icon">
+                <i className="fa-brands fa-github technologies_icon-img"></i>
+              </button>
+            </a>
+          </section>
+        </section>
+
+        <section className="infoAutor">
+          <div
+            className="infoAutor_image"
+            style={{ backgroundImage: `url(${avatarAutor})` }}
+          ></div>
+          <p className="infoAutor_job">{data.job || 'Full Stack Developer'}</p>
+          <p className="infoAutor_name">{data.autor || 'Emmelie Björklund'}</p>
+        </section>
+      </section>
+  )
+}
+
+export default Card
--- src/components/CardPreview.jsx
+++ src/components/CardPreview.jsx
@@ -1,11 +1,9 @@
 import PropTypes from 'prop-types';
 import '../styles/layout/CardPreview.scss';
-import defaultAvatar from '../assets/avatar.png';
-//import defaultProject from '../assets/cover.jpeg';
 import defaultProject from '../assets/brujafondo.jpg';
+import Card from './Card';
 
 const CardPreview = ({ data }) => {
-  const avatarAutor = data.image === '' ? defaultAvatar : data.image;
   const avatarProject = data.photo === '' ? defaultProject : data.photo;
   return (
     <section className="preview">
@@ -14,52 +12,7 @@ const CardPreview = ({ data }) => {
         style={{ backgroundImage: `url(${avatarProject})` }}
       ></div>
 
-      <section className="preview_autor">
-        <section className="infoProject">
-          <div className="infoProject_container">
-            <hr className="infoProject_container-line1" />
-            <p className="infoProject_container-subtitle">
-              Personal Project Card
-            </p>
-            <hr className="infoProject_container-line2" />
-          </div>
-
-          <h2 className="infoProject_title">
-            {data.name || 'Elegant Workspace'}
-          </h2>
-          <p className="infoProject_slogan">
-            {data.slogan || 'Diseños Exclusivos'}
-          </p>
-          <p className="infoProject_desc">
-            {data.desc ||
-              `Gestión de proyectos en línea, diseñado para simplificar y agilizar tu trabajo como programadora. Con solo crear una tarjeta para cada proyecto, podrás organizarlos de manera eficiente. ¡Optimiza tu productividad y alcanza tus metas de manera efectiva con nosotras!`}
-          </p>
-          <section className="technologies">
-            <p className="technologies_text">
-              {data.technologies || 'React JS - HTML - CSS'}
-            </p>
-            <a href={data.demo} target="_blank" rel="noreferrer">
-              <button className="technologies_icon">
-                <i className="fa-solid fa-globe technologies_icon-img"></i>
-              </button>
-            </a>
-            <a href={data.repo} target="_blank" rel="noreferrer">
-              <button className="technologies_icon">
-                <i className="fa-brands fa-github technologies_icon-img"></i>
-              </button>
-            </a>
-          </section>
-        </section>
-
-        <section className="infoAutor">
-          <div
-            className="infoAutor_image"
-            style={{ backgroundImage: `url(${avatarAutor})` }}
-          ></div>
-          <p className="infoAutor_job">{data.job || 'Full Stack Developer'}</p>
-          <p className="infoAutor_name">{data.autor || 'Emmelie Björklund'}</p>
-        </section>
-      </section>
+      <Card data={data}/>
     </section>
   );
 };
