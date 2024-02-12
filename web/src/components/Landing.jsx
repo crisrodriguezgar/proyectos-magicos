034503dc5294a9eca8d332308213b4ea1a2519a2
crisrodriguezgar <rodriguezgar.cris@gmail.com>
2024-02-12 19:13:16 +0100

    added Filter input

--- web/src/components/Landing.jsx
+++ web/src/components/Landing.jsx
@@ -3,11 +3,14 @@ import Btn from './Btn';
 import Card from './Card';
 import objectApi from '../services/api';
 import { useEffect, useState } from 'react';
-import objectToExport from '../services/localStorage';
 
-const Landing = () => {
+const Landing = ({ handleChange, projectFilter }) => {
   const [dataProjects, setDataProjects] = useState([]);
   const [allProjectsOptionSort, setAllProjectsOptionSort] = useState('asc');
+
+  const handleInput = (ev) => {
+    handleChange(ev.target.value);
+  };
 
   const handleAllProjectsOptions = (ev) => {
     const inputValue = ev.target.value;
@@ -19,13 +22,10 @@ const Landing = () => {
 
   const handleRemoveCard = async (projectId) => {
     console.log('estoy borrando');
-    // const message = await objectApi.deleteProject(projectId);
-    // console.log(message);
     objectApi.delete(projectId);
 
-    setDataProjects(
-      (projects) =>
-        projects.filter((proyect) => proyect.idProject !== projectId) //Después de eliminar el proyecto en la API, actualiza el estado dataProjects filtrando los proyectos para excluir el proyecto que acaba de ser eliminado.
+    setDataProjects(projects =>
+      projects.filter(proyect => proyect.idProject !== projectId)
     );
   };
 
@@ -35,32 +35,33 @@ const Landing = () => {
     };
     objectApi.get(params).then((response) => {
       const dataApi = response;
-      console.log(dataApi);
       setDataProjects(dataApi);
     });
   }, [allProjectsOptionSort]);
 
   const renderProjects = () => {
-    return dataProjects.map((project) => {
-      return (
-        <a
-          href={`https://proyectos-magicos.onrender.com/project/${project.idProject}`}
+    const filteredProjects = dataProjects.filter(project =>
+      project.name.toLowerCase().includes(projectFilter.toLowerCase())
+    );
+
+    return filteredProjects.map((project) => (
+      <a
+        href={`https://proyectos-magicos.onrender.com/project/${project.idProject}`}
+        key={project.idProject}
+        className="cardClick"
+      >
+        <Card
           key={project.idProject}
-          className="cardClick"
-        >
-          <Card
-            key={project.idProject}
-            data={project}
-            autorLanding="autorLanding"
-            titleLanding="titleLanding"
-            descLanding="descLanding"
-            sloganLanding="sloganLanding"
-            showIcon={true}
-            handleRemoveCard={handleRemoveCard}
-          />
-        </a>
-      );
-    });
+          data={project}
+          autorLanding="autorLanding"
+          titleLanding="titleLanding"
+          descLanding="descLanding"
+          sloganLanding="sloganLanding"
+          showIcon={true}
+          handleRemoveCard={handleRemoveCard}
+        />
+      </a>
+    ));
   };
 
   return (
@@ -68,41 +69,51 @@ const Landing = () => {
       <div className="divIntro">
         <Btn text={'Nuevo Proyecto'} route={'/project'} />
         <div className="divSort">
-          <label
-            className={`labelSort ${
-              allProjectsOptionSort === 'asc' ? 'clicked' : ''
-            }`}
-            htmlFor="sortAsc"
-          >
-            A-Z
-            <input
-              className="inputSort"
-              id="sortAsc"
-              type="radio"
-              name="sort"
-              value="asc"
-              checked={allProjectsOptionSort === 'asc'}
-              onChange={handleAllProjectsOptions}
-            />
-          </label>
+          <input
+            className="formInput"
+            type="text"
+            name="projects"
+            placeholder="Busca un proyecto..."
+            value={projectFilter}
+            onChange={handleInput}
+          />
+          <div>
+            <label
+              className={`labelSort ${
+                allProjectsOptionSort === 'asc' ? 'clicked' : ''
+              }`}
+              htmlFor="sortAsc"
+            >
+              A-Z
+              <input
+                className="inputSort"
+                id="sortAsc"
+                type="radio"
+                name="sort"
+                value="asc"
+                checked={allProjectsOptionSort === 'asc'}
+                onChange={handleAllProjectsOptions}
+              />
+            </label>
 
-          <label
-            className={`labelSort ${
-              allProjectsOptionSort === 'desc' ? 'clicked' : ''
-            }`}
-            htmlFor="sortDesc"
-          >
-            Z-A
-            <input
-              className="inputSort"
-              id="sortDesc"
-              type="radio"
-              name="sort"
-              value="desc"
-              checked={allProjectsOptionSort === 'desc'}
-              onChange={handleAllProjectsOptions}
-            />
-          </label>
+            <label
+              className={`labelSort ${
+                allProjectsOptionSort === 'desc' ? 'clicked' : ''
+              }`}
+              htmlFor="sortDesc"
+            >
+              Z-A
+              <input
+                className="inputSort"
+                id="sortDesc"
+                type="radio"
+                name="sort"
+                value="desc"
+                checked={allProjectsOptionSort === 'desc'}
+                onChange={handleAllProjectsOptions}
+              />
+            </label>
+          </div>
         </div>
       </div>
       <section className="preview previewLanding">{renderProjects()}</section>
