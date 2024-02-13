d26203dc4a958bba4bec979a4b02d84e18fc3e35
crisrodriguezgar <rodriguezgar.cris@gmail.com>
2024-02-13 12:31:10 +0100

    update .env

--- web/src/services/api.jsx
+++ web/src/services/api.jsx
@@ -1,7 +1,7 @@
 const postToApi = (data) => {
   // Llamamos a la API
   console.log(data);
-  return fetch('https://proyectos-magicos.onrender.com/createproject', {
+  return fetch('https://proyectosmagicos.onrender.com/createproject', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify(data),
@@ -16,7 +16,7 @@ const postToApi = (data) => {
 
 const getToApi = (params) => {
   return fetch(
-    `https://proyectos-magicos.onrender.com/listprojects?sort=${params.sort}`
+    `https://proyectosmagicos.onrender.com/listprojects?sort=${params.sort}`
   )
     .then((response) => response.json())
     .then((response) => {
